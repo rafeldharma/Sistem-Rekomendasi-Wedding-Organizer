9c820c3150ec8507aa2a88e0270946d4a370fd70
rafeldharma <147317672+rafeldharma@users.noreply.github.com>
2026-06-09 20:43:55 +0700

    Add files via upload

--- proses_sentimen.py
+++ proses_sentimen.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 from transformers import pipeline
 
-# 1. Load IndoBERT
+# 1. Load Model IndoBERT
 print("Sedang memuat model IndoBERT")
 nlp = pipeline("sentiment-analysis", model="indobenchmark/indobert-base-p2")
 
@@ -19,7 +19,7 @@ def clean_text(text):
 file_input = "Database_Gabungan_Lengkap.xlsx - Sheet1.csv"
 df = pd.read_excel("Database_Gabungan_Lengkap.xlsx")
 
-print("Membersihkan data")
+print("Sedang membersihkan data...")
 df['Isi_Ulasan_Clean'] = df['Isi Ulasan'].apply(clean_text)
 
 # 4. Proses Sentimen
@@ -27,7 +27,7 @@ def get_sentiment(text):
     if text == "":
         return "Netral"
     try:
-        result = nlp(text[:512])[0] # Limit di 512 karakter
+        result = nlp(text[:512])[0] # Limit 512 karakter
         label = result['label']
         # Labeling
         if label == "LABEL_0": return "Negatif"
@@ -35,10 +35,10 @@ def get_sentiment(text):
         return "Positif"
     except:
         return "Netral"
-
-print("Proses menganalisis sentimen")
+    
+print("Sedang menganalisis sentimen")
 df['Sentimen'] = df['Isi_Ulasan_Clean'].apply(get_sentiment)
 
-# 5. Hasil Akhir
+# 5. Simpan Hasil
 df.to_excel("Hasil_Sentimen_Final.xlsx", index=False)
 print("Selesai! File 'Hasil_Sentimen_Final.xlsx' telah dibuat.")
